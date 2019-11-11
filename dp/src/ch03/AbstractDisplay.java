9bdd99780c25480abac218e5bde2d53dcdbdbcf2
utg <utg@utg>
2019-11-11 10:51:49 +0900

    dp ch03

---
+++ dp/src/ch03/AbstractDisplay.java
@@ -0,0 +1,16 @@
+package ch03;
+//Abstract Class
+public abstract class AbstractDisplay {
+	public abstract void open();
+	public abstract void print();
+	public abstract void close();
+
+	//template method - use abstract method
+	public final void display() {
+		open();
+		for(int i = 0; i < 5; i++) {
+			print();
+		}
+		close();
+	}
+}
